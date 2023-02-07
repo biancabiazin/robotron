44b4692e7a5b512b445b364b79ccacaad6fbd877
Bianca <119738231+biancabiazin@users.noreply.github.com>
2023-02-07 17:39:42 -0300

    Add files via upload

---
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,7 @@
+const subtrair = document.querySelector("#bubtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+somar.addEventListener("click", (evento) => {
+    braco.value += 1
+});
